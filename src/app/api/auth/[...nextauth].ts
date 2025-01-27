9b16eb6defe5eae2d466db694477f5a55d29e14d
ZionGate Cwb <andre.lgnd17165@gmail.com>
2025-01-26 21:48:10 -0300

    modified:   src/app/api/auth/[...nextauth].ts

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
